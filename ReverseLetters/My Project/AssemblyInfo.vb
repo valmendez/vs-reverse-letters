3c3272def8bc7c20d45b5eef3597330637d2833d
Valeria <valeriam.bonilla07@gmail.com>
2022-04-08 17:20:15 -0400

    Add project files.

---
+++ ReverseLetters/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ReverseLetters")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ReverseLetters")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("85cb60e0-a5f3-4c34-b559-9dcdf959e49e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ ReverseLetters/frmBackward.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBackward
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblWord = New System.Windows.Forms.Label()
+        Me.txtWord = New System.Windows.Forms.TextBox()
+        Me.btnReverse = New System.Windows.Forms.Button()
+        Me.txtBackward = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblWord
+        '
+        Me.lblWord.AutoSize = True
+        Me.lblWord.Location = New System.Drawing.Point(110, 79)
+        Me.lblWord.Name = "lblWord"
+        Me.lblWord.Size = New System.Drawing.Size(61, 13)
+        Me.lblWord.TabIndex = 0
+        Me.lblWord.Text = "Enter word:"
+        '
+        'txtWord
+        '
+        Me.txtWord.Location = New System.Drawing.Point(177, 76)
+        Me.txtWord.Name = "txtWord"
+        Me.txtWord.Size = New System.Drawing.Size(100, 20)
+        Me.txtWord.TabIndex = 1
+        '
+        'btnReverse
+        '
+        Me.btnReverse.BackColor = System.Drawing.SystemColors.Desktop
+        Me.btnReverse.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReverse.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnReverse.Location = New System.Drawing.Point(113, 118)
+        Me.btnReverse.Name = "btnReverse"
+        Me.btnReverse.Size = New System.Drawing.Size(164, 35)
+        Me.btnReverse.TabIndex = 2
+        Me.btnReverse.Text = "Reverse Letters"
+        Me.btnReverse.UseVisualStyleBackColor = False
+        '
+        'txtBackward
+        '
+        Me.txtBackward.Location = New System.Drawing.Point(113, 170)
+        Me.txtBackward.Name = "txtBackward"
+        Me.txtBackward.ReadOnly = True
+        Me.txtBackward.Size = New System.Drawing.Size(164, 20)
+        Me.txtBackward.TabIndex = 3
+        '
+        'frmBackward
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtBackward)
+        Me.Controls.Add(Me.btnReverse)
+        Me.Controls.Add(Me.txtWord)
+        Me.Controls.Add(Me.lblWord)
+        Me.Name = "frmBackward"
+        Me.Text = "Write Backward"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblWord As Label
+    Friend WithEvents txtWord As TextBox
+    Friend WithEvents btnReverse As Button
+    Friend WithEvents txtBackward As TextBox
+End Class
